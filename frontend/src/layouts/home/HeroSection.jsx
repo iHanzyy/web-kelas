0ff6b2fd370f4dd1d97bd61d9ab50952af0bc8df
vathmos <esqeue@gmail.com>
2025-04-27 01:08:38 +0700

    add hero section

---
+++ frontend/src/layouts/home/HeroSection.jsx
@@ -0,0 +1,12 @@
+export default function HeroSection() {
+  return (
+    <div className="flex items-center justify-center flex-col w-full h-[calc(100vh-60px)]">
+      <p className="text-2xl">Hi</p>
+      <div className="">
+        <div className="text-8xl font-poppins-bold">WELCOME</div>
+        <div className="glow text-8xl font-poppins-bold absolute -translate-y-[96px]">WELCOME</div>
+      </div>
+      <p className="text-xl">TO XI RPL 1</p>
+    </div>
+  )
+}
