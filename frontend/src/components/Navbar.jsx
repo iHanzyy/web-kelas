75983fea957b99d9404de9e1e1b484c0ceb47348
vathmos <esqeue@gmail.com>
2025-04-27 01:06:48 +0700

    add navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <div className="flex h-[60px] bg-foreground justify-between ">
+      <div className="flex justify-baseline items-center">
+        <img src="/rpl-logo.svg" className="size-[38px] ml-[28px] cursor-pointer"/>
+      </div>
+      <div className=" flex text-primary font-semibold items-center gap-[60px] mr-[60px]">
+        <div className="cursor-pointer">About Us</div>
+        <div className="cursor-pointer">Structure & Schedule</div>
+        <div className="cursor-pointer">Memories</div>
+        <div className="cursor-pointer">Menfess</div>
+      </div>
+    </div>
+  )
+}
