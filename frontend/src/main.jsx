500eda8a87625014fd476920ad0ca9d19b0813a5
vathmos <esqeue@gmail.com>
2025-07-03 23:30:20 +0700

    add loader

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router';
 
+// Mount the app
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const loader = document.querySelector('.preloader');
+  if (loader) {
+    setTimeout(() => {
+      loader.remove();
+    }, 4000);
+  }
+});
