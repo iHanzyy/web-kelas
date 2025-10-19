3430094aebbe8ba0c38fab21455429eceea84449
vathmos <esqeue@gmail.com>
2025-10-19 21:17:43 +0700

    use cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const menfessRoutes = require("./routes/menfess.routes");
 const requireApiKey = require("./middleware/apiKey");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://baskom-rpl.vercel.app"],
+  }),
+);
 app.use(requireApiKey);
 
 app.get("/", (req, res) => {
