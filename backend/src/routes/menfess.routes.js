200e7c75cfd1af1bea7d092cbd0e0994684fd5ea
vathmos <esqeue@gmail.com>
2025-10-20 09:24:08 +0700

    add badwords checker

--- backend/src/routes/menfess.routes.js
+++ backend/src/routes/menfess.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const menfessController = require("../controllers/menfess.controller");
+const checkBadWords = require("../middleware/checkBadWords");
 
 const router = express.Router();
 
 router.get("/", menfessController.getAllMenfess);
 router.get("/:id", menfessController.getMenfess);
-router.post("/", menfessController.createMenfess);
+router.post("/", checkBadWords, menfessController.createMenfess);
 router.put("/:id", menfessController.updateMenfess);
 router.delete("/:id", menfessController.deleteMenfess);
 
