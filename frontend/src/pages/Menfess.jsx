18a35d47e923a2548181d990bf3f9681fc15872b
vathmos <esqeue@gmail.com>
2025-10-20 10:46:02 +0700

    send menfess from form to API

--- frontend/src/pages/Menfess.jsx
+++ frontend/src/pages/Menfess.jsx
@@ -2,11 +2,24 @@ import Footer from "../components/Footer";
 import GlowText from "../components/GlowText";
 import MenfessCard from "../components/MenfessCard";
 import Navbar from "../components/Navbar";
+import useCreateMenfess from "../hooks/useCreateMenfess";
 import useFetchMenfess from "../hooks/useFetchMenfess";
+import { useForm } from "react-hook-form";
 
 export default function Menfess() {
-  const { data: menfessData } = useFetchMenfess();
-  console.log(menfessData);
+  const { data: menfessData, refetch: refetchMenfess } = useFetchMenfess();
+  const { mutateAsync: createMenfess } = useCreateMenfess({
+    onSuccess: () => {
+      refetchMenfess();
+    },
+  });
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    createMenfess(data);
+    console.log(data);
+  };
+
   return (
     <div>
       <Navbar />
@@ -25,19 +38,30 @@ export default function Menfess() {
                 size="sm:text-primary text-white text-5xl"
                 font="font-poppins-semibold"
               />
-              <div className="mt-4  flex flex-col sm:flex-row justify-between bg-background-secondary w-[calc(100vw-50px)] max-w-[900px]  h-auto sm:h-[340px] rounded-[52px] p-7 sm:p-12 gap-4 sm:gap-6 md:gap-10">
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="mt-4  flex flex-col sm:flex-row justify-between bg-background-secondary w-[calc(100vw-50px)] max-w-[900px]  h-auto sm:h-[340px] rounded-[52px] p-7 sm:p-12 gap-4 sm:gap-6 md:gap-10"
+              >
                 <div className="flex flex-col items-center w-full sm:w-1/2 gap-4 sm:gap-8">
                   <div className="flex flex-nowrap gap-2 bg-input rounded-[52px] w-full py-3 px-5 overflow-hidden">
                     <div className="font-poppins-semibold text-nowrap">
                       FROM :{" "}
                     </div>
-                    <input type="text" className="w-9/12" />
+                    <input
+                      type="text"
+                      className="w-9/12"
+                      {...register("sender", { required: true })}
+                    />
                   </div>
                   <div className="flex flex-nowrap gap-2 bg-input rounded-[52px] w-full py-3 px-5">
                     <div className="font-poppins-semibold text-nowrap">
                       TO :{" "}
                     </div>
-                    <input type="text" className="w-10/12" />
+                    <input
+                      type="text"
+                      className="w-10/12"
+                      {...register("receiver", { required: true })}
+                    />
                   </div>
                   <button className="hidden sm:block bg-input rounded-[52px] w-fit py-2 px-6 cursor-pointer font-poppins-semibold">
                     SEND
@@ -49,12 +73,13 @@ export default function Menfess() {
                     name=""
                     id=""
                     className="h-full w-full resize-none"
+                    {...register("message", { required: true })}
                   ></textarea>
                 </div>
                 <button className="sm:hidden self-center bg-input rounded-[52px] w-fit py-1 px-5 cursor-pointer font-poppins-semibold">
                   SEND
                 </button>
-              </div>
+              </form>
             </div>
           </section>
           <div className="flex flex-col items-center bg-background sm:bg-transparent rounded-t-3xl sm:rounded-none pt-20 sm:pt-0 mt-10 gap-4">
